b7e5ab6fe6b3c2955a6592c70af00d5345179632
01joanna <joa.cuevas9@gmail.com>
2025-02-13 18:13:36 +0100

    changed nav

--- src/app/components/Navigation/Navigation.jsx
+++ src/app/components/Navigation/Navigation.jsx
@@ -1,13 +1,26 @@
 'use client'
 import React from 'react';
 import Data from '../../data/projects.json';
+import { motion } from 'framer-motion';
 
 export default function Navigation({ currentProject, setCurrentProject }) {
     return (
         <div className="absolute flex w-full h-full top-0 left-0 items-center justify-center z-20 gap-64">
-            <h1 className="titulo text-gray-200">{currentProject.name}</h1>
+            
+            {/* Animación para el título con framer-motion */}
+            <motion.h1
+                className="titulo text-gray-200"
+                key={currentProject.id}  // Cambiar cuando el proyecto cambia
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+                {currentProject.name}
+            </motion.h1>
+
             <div className="max-h-[6rem] w-[20rem] overflow-y-auto no-scrollbar">
-                <ul className="font-liberation uppercase text-sm text-white">
+                <ul className="font-liberation uppercase text-xs text-white">
                     {Data.projects.map((project) => (
                         <li key={project.id}>
                             <a
@@ -34,9 +47,16 @@ export default function Navigation({ currentProject, setCurrentProject }) {
             className='absolute bottom-20 left-3'
             id='intro-description'
             >
-                <p className='text-white text-xs font-liberation'>
+                <motion.p
+                    className='text-white text-xs font-liberation'
+                    key={currentProject.id}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
                     {currentProject.description}
-                </p>
+                </motion.p>
             </div>
         </div>
     );
--- src/app/components/Selected/Selected.jsx
+++ src/app/components/Selected/Selected.jsx
@@ -2,11 +2,11 @@
 
 export default function Selected() {
     return (
-        <section className="w-screen h-screen bg-blue-400 relative" id="selected-works">
+        <section className="w-screen h-screen bg-gray-500 relative" id="selected-works">
             <video></video>
             <nav className="absolute top-64">
                 <ul className="flex flex-row justify-evenly w-screen items-center">
-
+                    Aqui van los selected
                 </ul>
             </nav>
         </section>
