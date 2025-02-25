9c5986f59104aed2302f528170196aa53f8b37b8
01joanna <joa.cuevas9@gmail.com>
2025-02-25 15:35:38 +0100

    added video

--- src/app/components/Intro/Intro.jsx
+++ src/app/components/Intro/Intro.jsx
@@ -5,19 +5,19 @@ import Data from '../../data/projects.json';
 
 export default function Intro() {
     const [currentProject, setCurrentProject] = useState(Data.projects[0]);
+    
+    const general = Data.external[0].video;
 
     return (
         <section className="intro w-screen h-screen relative flex" id="home">
-            <video
+            <iframe
                 // src={currentProject.video}
-                className="w-full h-full object-cover"
-                type="video/mp4"
-                autoPlay
-                loop
-                muted
-                playsInline
-                pointer-events-none="true"
-            ></video>
+                src={general}
+                className="object-cover -mt-20"
+                // frameBorder="0"
+                // allow="autoplay; fullscreen"
+                // allowFullScreen
+            ></iframe>
             <Navigation
                 currentProject={currentProject}
                 setCurrentProject={setCurrentProject}
